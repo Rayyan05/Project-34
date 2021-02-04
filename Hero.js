53f78fe42d62aca5c91ec7891efc1910d9b53d36
Rayyan05 <75606877+Rayyan05@users.noreply.github.com>
2021-02-04 17:19:12 +0530

    Add files via upload

---
+++ Hero.js
@@ -0,0 +1,23 @@
+class Hero{
+    constructor(x,y,width,height){
+        var options = {
+            'isStatic': false,
+            'restitution': 1.0,
+           
+        }
+
+        this.body = Bodies.rectangle(x,y,width,height,options)
+        this.width = width
+        this.height = height
+        this.image =  loadImage("Superhero-01.png")
+        World.add(world,this.body)
+    }
+
+    display(){
+        var pos = this.body.position
+        var angle = this.body.angle
+
+        imageMode(CENTER)
+        image(this.image,pos.x,pos.y,this.width,this.height)
+    }
+}
